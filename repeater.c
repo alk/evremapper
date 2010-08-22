4cd27ca3026653c065078599024e33fb33478377
Aliaksey Kandratsenka <alk@tut.by>
2010-08-22 16:28:08 +0300

    handle pressing Fn when special key is pressed

--- repeater.c
+++ repeater.c
@@ -9,30 +9,32 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <pthread.h>
+#include <stdint.h>
 
 #include "keycodes.h"
 
 int source_evdev_fd;
 int uinput_fd;
+int verbose = 0;
 
 static inline
 int get_bit(void *_bits, int num)
 {
-	char *bits = _bits;
-	return (bits[num / 8] >> (num % 8)) & 1;
+	uint32_t *bits = _bits;
+	return (bits[num / 32] >> (num % 32)) & 1;
 }
 
 static inline
 void set_bit(void *_bits, int num, int value)
 {
-	char *bits = _bits;
-	char mask = (1 << (num % 8));
+	uint32_t *bits = _bits;
+	uint32_t mask = (1 << (num % 32));
 	value = (value) ? mask : 0;
-	char *ptr = bits + (num / 8);
-	char byte = *ptr;
-	byte &= ~mask;
-	byte |= value;
-	*ptr = byte;
+	uint32_t *ptr = bits + (num / 32);
+	uint32_t word = *ptr;
+	word &= ~mask;
+	word |= value;
+	*ptr = word;
 }
 
 static
@@ -76,7 +78,6 @@ int read_all(int fd, void *_buffer, size_t nbytes)
 	int total_readen = 0;
 	while (nbytes) {
 		int readen = read(fd, buffer, nbytes);
-		/* fprintf(stderr, "Read something (%d)\n", readen); */
 		if (!readen)
 			break;
 		if (readen < 0) {
@@ -116,6 +117,18 @@ char *events[EV_CNT] = {
 char *unknown_event_type = "-?-";
 
 static
+void print_key_name(uint16_t code)
+{
+	char *name = keycode_names[code];
+	char static_name[20];
+	if (!name) {
+		snprintf(static_name, sizeof(static_name), "x%04x", code);
+		name = static_name;
+	}
+	fputs(name, stdout);
+}
+
+static
 void pp_event(struct input_event *ev)
 {
 	char *event_type = events[ev->type];
@@ -126,21 +139,21 @@ void pp_event(struct input_event *ev)
 	printf(" %s: ", event_type);
 
 	if (ev->type == EV_KEY) {
-		char *name = keycode_names[ev->code];
-		char static_name[20];
-		if (!name) {
-			snprintf(static_name, sizeof(static_name), "x%04x", ev->code);
-			name = static_name;
-		}
-		printf("%s, 0x%08x\n", name, ev->value);
+		print_key_name(ev->code);
 	} else {
-		printf("0x%04x, 0x%08x\n", ev->code, ev->value);
+		printf("0x%04x", ev->code);
 	}
+	printf(", 0x%08x\n", ev->value);
 }
 
 static
 void write_event(struct input_event *ev)
 {
+	if (verbose) {
+		fputs("Output event: ", stdout);
+		pp_event(ev);
+	}
+
 	write(uinput_fd, ev, sizeof(*ev));
 	ev->type = EV_SYN;
 	ev->code = 0;
@@ -148,8 +161,63 @@ void write_event(struct input_event *ev)
 	write(uinput_fd, ev, sizeof(*ev));
 }
 
+#define MAP_SIZE 64
+uint32_t interesting_list_cnt;
+uint16_t interesting_list[MAP_SIZE];
+
+static
+uint16_t fn_keys_map[MAP_SIZE];
+
+static
+uint32_t interesting_keys[KEY_CNT/32];
+
+static
+uint16_t key_hash(uint16_t k)
+{
+	return (k % 255) % MAP_SIZE;
+}
+
+static
+void init_interesting_keys(void)
+{
+#define TR(a,b) do { \
+		set_bit(interesting_keys, a, 1);	\
+		uint16_t hash = key_hash(a);		\
+		if (fn_keys_map[hash])			\
+			abort();			\
+		interesting_list[interesting_list_cnt++] = a; \
+		fn_keys_map[hash] = b;			\
+	} while (0)
+
+	TR(KEY_BACKSPACE, KEY_DELETE);
+	TR(KEY_ENTER, KEY_INSERT);
+	TR(KEY_F1, KEY_BRIGHTNESSDOWN);
+	TR(KEY_F2, KEY_BRIGHTNESSUP);
+	TR(KEY_F3, KEY_SCALE);
+	TR(KEY_F4, KEY_DASHBOARD);
+	TR(KEY_F5, KEY_KBDILLUMDOWN);
+	TR(KEY_F6, KEY_KBDILLUMUP);
+	TR(KEY_F7, KEY_PREVIOUSSONG);
+	TR(KEY_F8, KEY_PLAYPAUSE);
+	TR(KEY_F9, KEY_NEXTSONG);
+	TR(KEY_F10, KEY_MUTE);
+	TR(KEY_F11, KEY_VOLUMEDOWN);
+	TR(KEY_F12, KEY_VOLUMEUP);
+	TR(KEY_UP, KEY_PAGEUP);
+	TR(KEY_DOWN, KEY_PAGEDOWN);
+	TR(KEY_LEFT, KEY_HOME);
+	TR(KEY_RIGHT, KEY_END);
+
+#undef TR
+}
+
 static
 int fn_pressed = 0;
+
+static
+uint32_t interesting_on[KEY_CNT/32];
+static
+uint32_t interesting_off[KEY_CNT/32];
 
 #define FN_KEY_CODE KEY_LEFTMETA
 #define NEW_LEFTMETA KEY_LEFTALT
@@ -158,14 +226,21 @@ int fn_pressed = 0;
 int translate_event(struct input_event *ev)
 {
 	if (ev->type != EV_KEY)
-		return 1;
+		return 0;
 
-	switch (ev->code) {
+	if (verbose) {
+		fputs("Input: ", stdout);
+		pp_event(ev);
+	}
+
+	uint16_t code = ev->code;
+
+	switch (code) {
 	case NEW_LEFTALT:
-		ev->code = KEY_LEFTALT;
+		code = ev->code = KEY_LEFTALT;
 		break;
 	case NEW_LEFTMETA:
-		ev->code = KEY_LEFTMETA;
+		code = ev->code = KEY_LEFTMETA;
 		break;
 	case KEY_BRIGHTNESSDOWN:
 	case KEY_BRIGHTNESSUP:
@@ -176,34 +251,54 @@ int translate_event(struct input_event *ev)
 		if (ev->value == 2)
 			return 0;
 		fn_pressed = ev->value;
+		/* int need_sync = 0; */
+		int i;
+		struct input_event ev2;
+		for (i=0;interesting_list[i];i++) {
+			uint16_t k = interesting_list[i];
+			if (get_bit(interesting_on, k)) {
+				if (verbose) {
+					fputs("Interesting key: ", stdout);
+					print_key_name(k);
+					putchar('\n');
+				}
+				set_bit(interesting_off, k, 1);
+				ev2 = *ev;
+				ev2.code = k;
+				if (!fn_pressed)
+					ev2.code = fn_keys_map[key_hash(k)];
+				ev2.value = 0;
+				write_event(&ev2);
+			}
+		}
+		memset(interesting_on, 0, sizeof(interesting_on));
 		return 0;
 	}
 
-	if (!fn_pressed)
-		return 1;
+	int is_interesting = get_bit(interesting_keys, code);
 
-	switch (ev->code) {
-#define TR(a,b) case a: ev->code = b; break
-		TR(KEY_BACKSPACE, KEY_DELETE);
-		TR(KEY_ENTER, KEY_INSERT);
-		TR(KEY_F1, KEY_BRIGHTNESSDOWN);
-		TR(KEY_F2, KEY_BRIGHTNESSUP);
-		TR(KEY_F3, KEY_SCALE);
-		TR(KEY_F4, KEY_DASHBOARD);
-		TR(KEY_F5, KEY_KBDILLUMDOWN);
-		TR(KEY_F6, KEY_KBDILLUMUP);
-		TR(KEY_F7, KEY_PREVIOUSSONG);
-		TR(KEY_F8, KEY_PLAYPAUSE);
-		TR(KEY_F9, KEY_NEXTSONG);
-		TR(KEY_F10, KEY_MUTE);
-		TR(KEY_F11, KEY_VOLUMEDOWN);
-		TR(KEY_F12, KEY_VOLUMEUP);
-		TR(KEY_UP, KEY_PAGEUP);
-		TR(KEY_DOWN, KEY_PAGEDOWN);
-		TR(KEY_LEFT, KEY_HOME);
-		TR(KEY_RIGHT, KEY_END);
-#undef TR
+	if (is_interesting) {
+		if (verbose) {
+			puts("This is interesting key");
+		}
+		if (!ev->value) {
+			set_bit(interesting_off, code, 0);
+			set_bit(interesting_on, code, 0);
+		} else {
+			if (get_bit(interesting_off, code)) {
+				if (verbose) {
+					fputs("Suppressing force-off key: ", stdout);
+					print_key_name(code);
+					putchar('\n');
+				}
+				return 0;
+			}
+			set_bit(interesting_on, code, 1);
+		}
+		if (fn_pressed)
+			ev->code = code = fn_keys_map[key_hash(code)];
 	}
+
 	return 1;
 }
 
@@ -232,7 +327,8 @@ void *events_eater(void *_arg)
 
 int main(int argc, char **argv)
 {
-	int verbose = 0;
+	init_interesting_keys();
+
 	if (argc >= 2 && strcmp(argv[1], "-v") == 0) {
 		argv++;
 		argc--;
@@ -272,12 +368,10 @@ int main(int argc, char **argv)
 	struct input_event ev;
 	while (1) {
 		read_all(source_evdev_fd, &ev, sizeof(ev));
-		if (ev.type == EV_SYN || ev.type == EV_MSC)
-			continue;
 		if (!translate_event(&ev))
 			continue;
-		if (verbose)
-			pp_event(&ev);
 		write_event(&ev);
+		if (verbose)
+			putchar('\n');
 	}
 }
